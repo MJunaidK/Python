3523eeb077a2eb7db0dac93ee80d86c6c229fa29
MJunaidK <junaidcts@gmail.com>
2025-07-13 16:24:34 -0400

    Class in Python

--- Day-17/main.py
+++ Day-17/main.py
@@ -3,11 +3,23 @@ class User:
         self.id = user_id
         self.username= username
         self.followers = 0
+        self.following = 0
+
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 user_1 = User("206027", "Junaid")
 user_2 = User("1206027", "Junaid Khan")
 print(user_1.id)
 print(user_2.id)
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
 
 
 
